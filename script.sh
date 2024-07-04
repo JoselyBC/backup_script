6d27c97757d161ec947c43f63ccfb501441d556d
JoselyBC <joselybcastro@gmail.com>
2024-07-04 15:52:05 -0300

    Atualização do scrip com a inclusão do registro de saída do comando RSYNC no caso de falha

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 SRC="/opt"
 DEST="/backup/opt"
 mkdir -p "$DEST"
 rsync -av --delete "$SRC/" "$DEST/"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup realizado com sucesso em $DEST" >> /var/log/backup_opt.log
+if [ $? -eq 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup realizado com sucesso em $DEST" >> /var/log/backup_opt.log
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Erro no backup do diretório $SRC para $DEST" >> /var/log/backup_opt.log
+fi
